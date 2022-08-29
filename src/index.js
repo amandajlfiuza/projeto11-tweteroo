f338d81f77f70922c476da7b8ee67da84a1909c8
amandajlfiuza <amandafiuza.pj@gmail.com>
2022-08-28 23:41:01 -0300

    post e get de tweets

--- src/index.js
+++ src/index.js
@@ -2,6 +2,39 @@ import express from 'express';
 import cors from 'cors';
 
 const server = express();
+server.use(express.json());
+server.use(cors());
+
+const users = [];
+const tweets = [];
+
+server.post('/sign-up', (req, res) => {
+    const newUser = req.body;
+    users.push({
+        ...newUser,
+        id: users.length + 1,
+    });
+    res.send('OK');
+});
+
+server.post('/tweets', (req, res) => {
+    const newTweet = req.body;
+    tweets.push({
+        ...newTweet,
+        id: tweets.length + 1,
+    });
+    res.send('OK');
+});
+
+server.get('/tweets', (req, res) => {
+    const lastTweets = tweets.slice(tweets.length - 10);
+    const displayedTweets = lastTweets.map(tweet => {
+        const userObj = users.find(value => value.username === tweet.username);
+        const user = userObj.avatar;
+        return ({...tweet, avatar: user});
+    });
+    res.send(displayedTweets);
+});
 
 server.listen(5000, () => {
     console.log('Listening on port 5000');
